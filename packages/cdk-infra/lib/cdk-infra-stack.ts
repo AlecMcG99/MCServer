cbd649cb97e2524d3f0551db0fd968958b8fea89
AlecMcG99 <alecmcglasson@gmail.com>
2022-02-07 17:42:06 -0600

    Set up ec2 instances

--- packages/cdk-infra/lib/cdk-infra-stack.ts
+++ packages/cdk-infra/lib/cdk-infra-stack.ts
@@ -1,15 +1,27 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { Instance, InstanceType, MachineImage, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
+
 export class CdkInfraStack extends Stack {
+  readonly vanillaServerEC2: Instance;
+  readonly hexxitServerEC2: Instance;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const serverVpc = new Vpc(this, "serverVpc");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.vanillaServerEC2 = new Instance(this, "Minecraft Server", {
+      instanceType: new InstanceType("t2.medium"),
+      vpc: serverVpc,
+      machineImage: MachineImage.latestAmazonLinux()
+    });
+
+    this.hexxitServerEC2 = new Instance(this, "Hexxit Server", {
+      instanceType: new InstanceType("t3.medium"), 
+      vpc: serverVpc,
+      machineImage: MachineImage.latestAmazonLinux()
+    });
   }
 }
