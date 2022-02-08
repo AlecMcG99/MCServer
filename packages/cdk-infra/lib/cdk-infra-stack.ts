6ce565ab892dfbe2584b5d9f24b2c4781f39e268
AlecMcG99 <alecmcglasson@gmail.com>
2022-02-07 18:50:52 -0600

    removed security groups

--- packages/cdk-infra/lib/cdk-infra-stack.ts
+++ packages/cdk-infra/lib/cdk-infra-stack.ts
@@ -15,15 +15,13 @@ export class CdkInfraStack extends Stack {
     this.vanillaServerEC2 = new Instance(this, "Minecraft Server", {
       instanceType: new InstanceType("t2.medium"),
       vpc: serverVpc,
-      machineImage: MachineImage.latestAmazonLinux(), 
-      securityGroup: SecurityGroup.fromSecurityGroupId(this, "mcSecurityGroup", 'sg-08aa5f2fb753f312c', {mutable: false})
+      machineImage: MachineImage.latestAmazonLinux()
     });
 
     this.hexxitServerEC2 = new Instance(this, "Hexxit Server", {
       instanceType: new InstanceType("t3.medium"), 
       vpc: serverVpc,
-      machineImage: MachineImage.latestAmazonLinux(), 
-      securityGroup: SecurityGroup.fromSecurityGroupId(this, "mcSecurityGroup", 'sg-08aa5f2fb753f312c', {mutable: false})
+      machineImage: MachineImage.latestAmazonLinux()
     });
   }
 }
